523944c14f9b5a9c748d9bc53b0ba944c8af8b07
Nathania Edrea Haryanto <nathania.e.haryanto@stud.h-da.de>
2022-04-05 18:48:35 +0200

    correctly display data on result page

--- client/src/pages/Result.js
+++ client/src/pages/Result.js
@@ -2,22 +2,31 @@ import React, { useEffect, useState } from "react";
 import { useHistory, useLocation, useParams } from "react-router-dom";
 
 function Result() {
-  let [data, setData] = useState({});
-  let { job, location } = useParams();
+  const [data, setData] = useState({
+    title: "",
+    location: "",
+    salary: 0,
+    malePercent: 0,
+    femalePercent: 0,
+    matLeave: 0,
+  });
+  const { job, location } = useParams();
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(
         `/jobs?title=${job.toLocaleLowerCase()}&location=${location.toLowerCase()}`
       );
       const jsonData = await res.json();
+      // console.log(jsonData.jobs[0]);
       setData(jsonData.jobs[0]);
+      // console.log(data);
     };
     fetchData();
   }, []);
   return (
     <div>
       <h1>
-        {data.title.toUpperCase()} - {data.location.toUpperCase()}
+        {data.title} - {data.location}
       </h1>
       <section>
         <h2>Average Salary: </h2>
